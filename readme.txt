04cf3934b51e625afa53b9d4e86fd94ab66ecda8
Maciej Gryka <maciej.gryka@gmail.com>
2010-11-29 11:07:24 +0000

    added some license disclaimers

--- readme.txt
+++ readme.txt
@@ -4,7 +4,8 @@ made by maciejgryka.com
 28/11/2010
 
 sfgiff uses YQL to process an SQL-like query and get JSON data about matching Flickr photos.
-The photos are then downloaded and placed in images/ directory next to the script file.
+The photos are then downloaded and placed in images/ directory next to the script file. They are named as follows: owner_id_secret_suffix.jpg
+To see the photo on the authors page go to http://www.flickr.com/photos/[owner]/[id]
 
 To get the specific images you need, modify the query as well as im_from and im_to to change the number of photos returned. 
 
--- sfgiff.py
+++ sfgiff.py
@@ -1,15 +1,30 @@
+# Disclaimer: this script uses the Flickr API but is not endorsed or certified by Flickr.
+# It was made for research purposes only.
+#
+# If you run it make sure to only use the downloaded images according to their license:
+# 1	Attribution-NonCommercial-ShareAlike License 	http://creativecommons.org/licenses/by-nc-sa/2.0/
+# 2	Attribution-NonCommercial License 				http://creativecommons.org/licenses/by-nc/2.0/
+# 3	Attribution-NonCommercial-NoDerivs License		http://creativecommons.org/licenses/by-nc-nd/2.0/
+# 4	Attribution License								http://creativecommons.org/licenses/by/2.0/ 
+# 5	Attribution-ShareAlike License					http://creativecommons.org/licenses/by-sa/2.0/
+# 6	Attribution-NoDerivs License					http://creativecommons.org/licenses/by-nd/2.0/
+# 7	No known copyright restrictions					http://flickr.com/commons/usage/
+#
+# maciejgryka.com
+#
+
 import urllib
 import json
 import os
 import sys
 import time
-from datetime import date
+# from datetime import date
 
 from IPython.Debugger import Tracer; debug_here = Tracer()
 
-# how may images should we get?
+# how may images should we get? (maximum 5000 with a single query)
 im_from = '0'
-im_to = '15'
+im_to = '20'
 
 if (int(im_to) - int(im_from) > 5000):
 	print 'error: maximum of 5000 images per query!'
@@ -28,13 +43,11 @@ im_suffix = '_b'
 yql_url = 'http://query.yahooapis.com/v1/public/yql?q='
 
 # the YQL yery - change it to reflect the images you want
-query = 'SELECT id, secret, farm, server '\
+query = 'SELECT id, secret, farm, server, owner '\
 		'FROM flickr.photos.search(' + im_from + ',' + im_to + ') '\
 		'WHERE text = "shadow" '\
 		'AND license = "1,2,4,5,7" '\
 #		'AND min_taken_date = "' + date(2001, 05, 05).isoformat() + '"'
-
-print query
 
 query_url = yql_url + urllib.quote(query) + '&format=json'
 
@@ -58,11 +71,11 @@ for i, im in enumerate(jsonresult['query']['results']['photo']):
 	t0 = time.time()
 	im_url = "http://farm" + im['farm'] + '.static.flickr.com/' + im['server'] + '/' + im['id'] + '_' + im['secret'] + im_suffix + '.jpg'
 	image = urllib.URLopener()
-	image.retrieve(im_url, 'images\\' + im['id'] + '_' + im['secret'] + im_suffix + '.jpg')
+	image.retrieve(im_url, 'images\\' + im['owner'] + '_' + im['id'] + '_' + im['secret'] + im_suffix + '.jpg')
 	t_total += time.time() - t0		# total time spent downloading so far + time taken by last download
 	secs_left = (int(n_images) - i+1) * t_total/(i+1)	# number of images left * average download time
 	time_left_str = ''
-	if (i % 5 == 0):
+	if (i % 10 == 0):
 		h = int(secs_left) / 3600
 		if (h > 0):
 			time_left_str += '%ih '%(h)
