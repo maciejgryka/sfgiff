eded4c6329efc63b9d0846a39687275f18a9cb95
Maciej Gryka <maciej.gryka@gmail.com>
2010-11-29 10:37:01 +0000

    added prettier predicted time output, more comments and fixed some typos

--- readme.txt
+++ readme.txt
@@ -3,12 +3,12 @@ sfgiff.py - Script For Getting Images From Flickr
 made by maciejgryka.com
 28/11/2010
 
-sfgiff uses YQL to process an SQL-like query and get JSON data describing matching Flickr photos.
+sfgiff uses YQL to process an SQL-like query and get JSON data about matching Flickr photos.
 The photos are then downloaded and placed in images/ directory next to the script file.
 
-To get the images you want modify the query as well as im_from and im_to limit the number of photos you want. 
+To get the specific images you need, modify the query as well as im_from and im_to to change the number of photos returned. 
 
 Note that: 
-- YQL restrics the maximum number of results to 5000 - if you want more, you will need to process separate queries.
-- all the images are have a maximum dimension of 1024px; to get the original size you need to find o-secret (original secret) and I'm not sure how
+- YQL restrics the maximum number of results to 5000 per query - if you need more images, you will need to process multiple queries
+- all the images are have a maximum dimension of 1024px; to get the original size you will need to find o-secret (original secret) and I'm not sure how
 
--- sfgiff.py
+++ sfgiff.py
@@ -9,9 +9,23 @@ from IPython.Debugger import Tracer; debug_here = Tracer()
 
 # how may images should we get?
 im_from = '0'
-im_to = '20'
+im_to = '15'
 
-yql_url = 'http://query.yahooapis.com/v1/public/yql?'
+if (int(im_to) - int(im_from) > 5000):
+	print 'error: maximum of 5000 images per query!'
+	sys.exit(0)
+
+# suffix controlling the size of the image
+# _s	small square 75x75
+# _t	thumbnail, 100 on longest side
+# _m	small, 240 on longest side
+#  	 	(empty) medium, 500 on longest side
+# _z	medium 640 on longest side
+# _b	large, 1024 on longest side
+# _o	(needs special secret) original image, either a jpg, gif or png, depending on source format
+im_suffix = '_b'
+
+yql_url = 'http://query.yahooapis.com/v1/public/yql?q='
 
 # the YQL yery - change it to reflect the images you want
 query = 'SELECT id, secret, farm, server '\
@@ -19,8 +33,10 @@ query = 'SELECT id, secret, farm, server '\
 		'WHERE text = "shadow" '\
 		'AND license = "1,2,4,5,7" '\
 #		'AND min_taken_date = "' + date(2001, 05, 05).isoformat() + '"'
-		
-query_url = yql_url + 'q=' + urllib.quote(query) + '&format=json'
+
+print query
+
+query_url = yql_url + urllib.quote(query) + '&format=json'
 
 print 'getting JSON response...'
 jsonfile = urllib.urlopen(query_url)
@@ -36,16 +52,26 @@ if (n_images == '0'):
 if (not(os.path.exists('images/'))):
 	os.mkdir('images')
 
-print 'downloading %s files to finish in approximately...'%(n_images)
+print 'downloading %s files...'%(n_images)
 t_total = 0
 for i, im in enumerate(jsonresult['query']['results']['photo']):
 	t0 = time.time()
-	i += 1	# first image should have index 1 etc.
-	im_url = "http://farm" + im['farm'] + '.static.flickr.com/' + im['server'] + '/' + im['id'] + '_' + im['secret'] + '_b.jpg'
+	im_url = "http://farm" + im['farm'] + '.static.flickr.com/' + im['server'] + '/' + im['id'] + '_' + im['secret'] + im_suffix + '.jpg'
 	image = urllib.URLopener()
-	image.retrieve(im_url, 'images\\' + im['id'] + '_' + im['secret'] + '_b.jpg')
-	t_total += time.time() - t0							# total time spent downloading so far
-	secs_left = int(int(n_images) - i) * t_total/(i)	# number of images left * average download time
-	if ((i-1) % 5 == 0):
-		m = secs_left / 60
-		print '%im %.0fs'%(m, secs_left%60)
+	image.retrieve(im_url, 'images\\' + im['id'] + '_' + im['secret'] + im_suffix + '.jpg')
+	t_total += time.time() - t0		# total time spent downloading so far + time taken by last download
+	secs_left = (int(n_images) - i+1) * t_total/(i+1)	# number of images left * average download time
+	time_left_str = ''
+	if (i % 5 == 0):
+		h = int(secs_left) / 3600
+		if (h > 0):
+			time_left_str += '%ih '%(h)
+			secs_left = secs_left - h*3600
+			
+		m = int(secs_left) / 60
+		if (m > 0):
+			time_left_str += '%im '%(m)
+			secs_left = secs_left - m*60
+
+		print time_left_str + '%.0fs'%(secs_left)
+print 'done!'
